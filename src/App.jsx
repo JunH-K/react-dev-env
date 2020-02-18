82bcfe8fb7b9c82288d859c30f1f0f036e379f74
JunH-K <k.developer@kakao.com>
2020-02-18 18:08:45 +0900

    css 위치변경

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from 'reducers';
-import './css/reset.css';
+import '../public/css/reset.css';
 import MainContainer from 'Containers/MainContainer';
 
 const store = createStore(rootReducer, composeWithDevTools());
