9c802fb7b6fcf60698513029d800ff4aa9947763
JunH-K <k.developer@kakao.com>
2020-02-19 21:27:32 +0900

    babel 설정 분리

--- webpack.config.js
+++ webpack.config.js
@@ -18,25 +18,7 @@ const config = {
     rules: [
       {
         test: /\.jsx?/,
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                targets: {
-                  browsers: ['>1% in KR', 'last 2 chrome versions'],
-                },
-                debug: false,
-              },
-            ],
-            '@babel/preset-react',
-          ],
-          plugins: [
-            '@babel/plugin-proposal-class-properties',
-            'react-hot-loader/babel',
-          ],
-        },
+        loader: 'babel-loader'
       },
       {
         test: /\.css$/i,
