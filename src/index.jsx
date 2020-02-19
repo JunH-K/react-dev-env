53d402855f8bba2ee41053e7ff0b0c5f52e6ccaf
JunH-K <k.developer@kakao.com>
2020-02-19 17:26:51 +0900

    remove log

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { hot } from 'react-hot-loader/root';
-
-console.log(hot);
 import App from 'App';
 
-console.log(App);
 const Hot = hot(App);
 
 ReactDom.render(<Hot />, document.querySelector('#root'));
