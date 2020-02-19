62e124c290d6f6d2a7a7cd8292fb122563d32807
JunH-K <k.developer@kakao.com>
2020-02-19 22:00:01 +0900

    feat : redux-saga 설치및 기본 셋

--- src/store/middleware.js
+++ src/store/middleware.js
@@ -1,14 +1,14 @@
 import { applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createLogger } from 'redux-logger';
-
-// https://www.npmjs.com/package/redux-devtools-extension
+import createSagaMiddleware from 'redux-saga';
 
 const logger = createLogger();
+export const sagaMiddleware = createSagaMiddleware();
 
 const configureMiddleware = () => {
   const isDev = process.env.NODE_ENV === 'development';
-  const middleware = [];
+  const middleware = [sagaMiddleware];
 
   if (isDev) {
     const devMiddleware = [...middleware, logger];
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,10 @@
 import { createStore } from 'redux';
 import rootReducer from 'reducers';
 import middleware from 'store/middleware';
+import { sagaMiddleware } from './middleware';
+import rootSaga from 'sagas';
 
 const store = createStore(rootReducer, middleware());
+sagaMiddleware.run(rootSaga);
 
 export default store;
