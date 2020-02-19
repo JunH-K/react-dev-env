8d7c219ca5e6cbfb49eb7556df8a93f3cabad2fb
JunH-K <k.developer@kakao.com>
2020-02-19 15:01:56 +0900

    feat : middlewares 설정 추가

--- src/store/middlewares/middlewares.js
+++ src/store/middlewares/middlewares.js
@@ -2,16 +2,7 @@ import { applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 // https://www.npmjs.com/package/redux-devtools-extension
 
-const logger = store => next => action => {
-  console.log('현재상태->', store.getState());
-  console.log('action->', action);
-  const result = next(action);
-  console.log('다음상태->', store.getState());
-  console.log('\n');
-  return result;
-};
-
-const middlewares = [logger];
+const middlewares = [];
 
 const configureMiddleware = () => {
   const isDev = process.env.NODE_ENV === 'development';
