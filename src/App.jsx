fcf96cdc07ffef80d972da40bc6bc74ff56a8fe8
JunH-K <k.developer@kakao.com>
2020-02-19 22:19:35 +0900

    feat : 폴더명 소문자일관성

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import '../public/static/css/reset.css';
 import store from 'store/store';
-import MainContainer from 'Containers/MainContainer';
+import MainContainer from 'containers/MainContainer';
 
 const App = () => {
   return (
